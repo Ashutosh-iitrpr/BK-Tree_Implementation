effe55f3b64b30b796a7a151f10e915206a3ccad
Ashutosh-iitrpr <141392925+Ashutosh-iitrpr@users.noreply.github.com>
2024-10-23 23:13:52 +0530

    Update BK_structure.c

--- BK_structure.c
+++ BK_structure.c
@@ -40,36 +40,7 @@ struct Node* createBKTree(const char *word)
     return root;
 }
 
-//function to calculate the levenshtein distance between two strings
-int levenshteinDistance(const char *word1, const char *word2)
-{
-    int len1 = strlen(word1);
-    int len2 = strlen(word2);
-    int dp[len1 + 1][len2 + 1];
-    for(int i = 0; i <= len1; i++)
-    {
-        for(int j = 0; j <= len2; j++)
-        {
-            if(i == 0)
-            {
-                dp[i][j] = j;
-            }
-            else if(j == 0)
-            {
-                dp[i][j] = i;
-            }
-            else if(word1[i - 1] == word2[j - 1])
-            {
-                dp[i][j] = dp[i - 1][j - 1];
-            }
-            else
-            {
-                dp[i][j] = 1 + min(dp[i - 1][j - 1], min(dp[i - 1][j], dp[i][j - 1]));
-            }
-        }
-    }
-    return dp[len1][len2];
-}
+
 
 //function to insert a new node in BK tree
 void insertNode(struct Node *root, const char *word)
@@ -78,7 +49,7 @@ void insertNode(struct Node *root, const char *word)
     int distance = 0;
     while(currentNode != NULL)
     {
-        distance = levenshteinDistance(currentNode->word, word);
+        distance = lev_dist(currentNode->word, word);
         if(currentNode->children[distance] == NULL)
         {
             currentNode->children[distance] = createNode(word);
