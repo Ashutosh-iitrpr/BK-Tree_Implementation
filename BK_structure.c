ced13dfd526617ee95d9d4142437bbc35ab26fc9
Ashutosh-iitrpr <141392925+Ashutosh-iitrpr@users.noreply.github.com>
2024-10-24 16:40:33 +0530

    added depth traversal

--- BK_structure.c
+++ BK_structure.c
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
 #include <limits.h>
 #include <time.h>
 
@@ -19,7 +20,7 @@ struct Node
 };
 
 //function to create a new node
-struct Node* createNode(const char *word)
+struct Node* createNode(char *word)
 {
     struct Node *newNode = (struct Node*)malloc(sizeof(struct Node));
     newNode->length = strlen(word);
@@ -34,7 +35,7 @@ struct Node* createNode(const char *word)
 }
 
 //function to create a BK tree
-struct Node* createBKTree(const char *word)
+struct Node* createBKTree(char *word)
 {
     struct Node *root = createNode(word);
     return root;
@@ -43,7 +44,7 @@ struct Node* createBKTree(const char *word)
 
 
 //function to insert a new node in BK tree
-void insertNode(struct Node *root, const char *word)
+void insertNode(struct Node *root,  char *word)
 {
     struct Node *currentNode = root;
     int distance = 0;
@@ -62,6 +63,35 @@ void insertNode(struct Node *root, const char *word)
     }
 }
 
+void depth_traverse(struct Node *root)
+{
+    if(root == NULL)
+    {
+        return;
+    }
+    for(int i = 0; i < root->numChildren; i++)
+    {
+        depth_traverse(root->children[i]);
+    }
+    printf("%s\n", root->word);
+}
+
+
+
 int main()
 {
+    struct Node *root = createBKTree("hello");
+    insertNode(root, "world");
+    insertNode(root, "hell");
+    insertNode(root, "help");
+    insertNode(root, "jello");
+    insertNode(root, "jelly");
+    insertNode(root, "jellybean");
+    insertNode(root, "jellybeans");
+    insertNode(root, "jellyfish");
+    insertNode(root, "jellyfishes");
+
+    depth_traverse(root);
+    
+    return 0;
 }
