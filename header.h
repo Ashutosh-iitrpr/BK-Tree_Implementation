db21648130777bb40b1a3742d3d11cbf01c37c2c
Ashutosh-iitrpr <141392925+Ashutosh-iitrpr@users.noreply.github.com>
2024-10-25 11:05:18 +0530

    Update header.h

--- header.h
+++ header.h
@@ -4,4 +4,15 @@
 int min(int a, int b, int c);
 int lev_dist(char* a, char* b);
 
+#define NEG_INF -100000
+#define MAX(a,b) (((a)>(b))?(a):(b))
+#define MIN(a,b) (((a)<(b))?(a):(b))
+
+int abs(int a);
+void evaluate(int** f, char* a, char* b, int k, int p);
+int edit_dist(int** f, char* a, char* b);
+int UK_lev_dist(char* a, char* b);
+
+char **array_maker(char *filename);
+
 #endif
