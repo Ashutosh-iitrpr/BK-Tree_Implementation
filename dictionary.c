9236211b5e81bd276fd6f960d362c79b03c5b279
Ashutosh-iitrpr <141392925+Ashutosh-iitrpr@users.noreply.github.com>
2024-10-25 17:02:25 +0530

    dictionary loader function added

--- dictionary.c
+++ dictionary.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "header.h"
 
-char **array_maker(char *filename) {
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "header.h"
+
+char **array_maker(char *filename, int *count) {
     FILE *fp = fopen(filename, "r");
     if (fp == NULL) {
         perror("Error opening file");
@@ -10,18 +17,20 @@ char **array_maker(char *filename) {
     }
 
     char line[100];
-    int count = 0;
+    *count = 0;
 
     // Count the number of lines in the file
     while (fgets(line, sizeof(line), fp) != NULL) {
-        count++;
+        (*count)++;
     }
-    printf("The number of lines in the file is: %d\n", count);
+
+    printf("The number of lines in the file is: %d\n", *count);
+
     // Rewind the file pointer to the beginning
     rewind(fp);
 
     // Allocate memory for the array of strings
-    char **array = (char **)malloc(sizeof(char *) * count);
+    char **array = (char **)malloc(sizeof(char *) * (*count));
     if (array == NULL) {
         perror("Error allocating memory");
         fclose(fp);
@@ -33,7 +42,6 @@ char **array_maker(char *filename) {
     while (fgets(line, sizeof(line), fp) != NULL) {
         // Remove the trailing newline character
         line[strcspn(line, "\n")] = 0;
-
         // Allocate memory for the string
         array[i] = (char *)malloc(sizeof(char) * (strlen(line) + 1));
         if (array[i] == NULL) {
@@ -45,7 +53,6 @@ char **array_maker(char *filename) {
             free(array);
             return NULL;
         }
-
         // Copy the line to the array
         strcpy(array[i], line);
         i++;
@@ -53,15 +60,26 @@ char **array_maker(char *filename) {
 
     // Close the file
     fclose(fp);
-
     return array;
 }
-// int main(){
-    
-//     char **array = array_maker("dictionary.txt");
-//     if (array == NULL) {
-//         return 1;
-//     }
 
-//     return 0;
-// }
+struct Node* dic_loader(char *filename) {
+    int count;
+    char **dictionary = array_maker(filename, &count);
+    if (dictionary == NULL) {
+        return NULL;
+    }
+
+    struct Node *root = createBKTree(dictionary[0]);
+
+    for (int i = 1; i < count; i++) {
+        insertNode(root, dictionary[i]);
+    }
+
+    for (int i = 0; i < count; i++) {
+        free(dictionary[i]);
+    }
+    free(dictionary);
+    return root;
+}
+
